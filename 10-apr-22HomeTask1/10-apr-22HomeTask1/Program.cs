b1ef75d8b958289ae51ae93b04b44808a9a1f1ec
reshad <you@example.com>
2022-04-11 01:49:22 +0400

    first commit

---
+++ 10-apr-22HomeTask1/10-apr-22HomeTask1/Program.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10_apr_22HomeTask1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HttpClient cilent = new HttpClient();
+            string response = cilent.GetStringAsync("https://www.cbar.az/currencies/10.04.2022.xml").Result;
+            // Console.WriteLine(response);
+            //Console.ReadLine();
+
+            string resJson = JsonConvert.SerializeObject(response);
+
+            StreamWriter sw = new StreamWriter(@"C:\Users\comp\source\repos\10-apr-22HomeTask1\10-apr-22HomeTask1\File\main.json");
+            sw.Write(resJson);
+            sw.Close();
+        }
+    }
+}
---
+++ 10-apr-22HomeTask2/10-apr-22HomeTask2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10_apr_22HomeTask2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HttpClient client = new HttpClient();
+            string response = client.GetStringAsync("https://jsonplaceholder.typicode.com/posts").Result;
+
+            Console.WriteLine(response);
+            Console.ReadLine();
+
+
+
+
+        }
+    }
+}
